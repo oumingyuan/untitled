51f713f6dc6ffc8dd6377bbde04a8ac4abdd1889
oumingyuan <oumingyuan@live.com>
2019-05-17 17:04:19 +0800

    Initial commit

---
+++ world.js
@@ -0,0 +1,57 @@
+const obj = {
+    'age': '18',
+    'name': 'oumingyuan',
+    'language': {
+        'first': 'chinese',
+        'second': 'english',
+        'computer': {
+            'first': 'java',
+        }
+    }
+};
+
+
+/**
+ * 参数是 map
+ * @param obj
+ * @returns {Array}
+ */
+function convert(obj) {
+    let data_list = [];
+
+    //第一层循环
+    Object.keys(obj).forEach(function (key) {
+        let data_map = {};
+        data_map.label = key;
+        /**
+         * 设置children分为两种情况
+         */
+        if (typeof obj[key] === 'object') {
+            let object2 = obj[key];
+            let list2 = [];
+
+
+
+            Object.keys(object2).forEach(function (key2) {
+                let data_map2 = {};
+                data_map2.label = key2;
+                data_map2.children = [{label: object2[key2]}];
+                list2.push(data_map2)
+            });
+            //设置children
+            data_map.children = list2;
+        } else {
+            //设置children
+            data_map.children = [{label: obj[key]}];
+        }
+
+
+        // 组装
+        data_list.push(data_map)
+    });
+    return data_list;
+}
+
+let result = convert(obj);
+
+console.log(result);
